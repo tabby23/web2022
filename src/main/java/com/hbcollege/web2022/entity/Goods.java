10d143f1a59380d359955f6e66881870fdac537a
Rick <tabby23@163.com>
2021-12-30 17:00:06 +0800

    lombok

--- src/main/java/com/hbcollege/web2022/entity/Goods.java
+++ src/main/java/com/hbcollege/web2022/entity/Goods.java
@@ -1,31 +1,12 @@
 package com.hbcollege.web2022.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Goods {
     private String name;
     private String pic;
     private float price;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPic() {
-        return pic;
-    }
-
-    public void setPic(String pic) {
-        this.pic = pic;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
 }
